8717019362efa7bb9cc17308f75ebd4246b12860
Mahrukh Nasir <choudary770@gmail.com>
2025-03-16 13:03:59 -0700

    php files creation

---
+++ phpPractice/forget.php
@@ -0,0 +1,3 @@
+<?php include 'includes/header.php'; ?>
+    <h1>Forget</h1>
+<?php include 'includes/footer.php'; ?>
---
+++ phpPractice/includes/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+<div class="container">
+  <a class="navbar-brand text-white" href="home.php"> <i class="fa fa-shield me-2"></i>Security System</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link text-white" href="home.php"> <i class="fa fa-home me-2"></i>Home </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-user-circle-o me-2"></i> Profile
+        </a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="index.php">Login</a></li>
+          <li><a class="dropdown-item" href="register.php">Register</a></li>
+        </ul>
+      </li>
+    </ul>
+    
+  </div>
+  </div>
+</nav>
+</body>
+</html>
